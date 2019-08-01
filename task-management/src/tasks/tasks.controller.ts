5b4ad0a1f35d6a32737f5924563c0dfd84e471f5
saibot777 <stefantrajkovic777@gmail.com>
2019-08-01 13:45:34 +0200

    refactored using Dto

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,10 +13,7 @@ export class TasksController {
     }
 
     @Post()
-    createTask(
-        @Body('title') title,
-        @Body('description') description,
-    ): void {
-        this.tasksService.createTask(title, description);
+    createTask(@Body() createTaskDto: CreateTaskDto): void {
+        this.tasksService.createTask(createTaskDto);
     }
 }
