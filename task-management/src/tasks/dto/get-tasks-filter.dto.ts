ec3d26dc42afa03d9ad1d8d63dee46f8d6b16fea
saibot777 <stefantrajkovic777@gmail.com>
2019-09-18 15:27:37 +0200

    filter validation impl

--- task-management/src/tasks/dto/get-tasks-filter.dto.ts
+++ task-management/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '../task.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDto {
-    status: TaskStatus;
-    search: string;
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
+  status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  search: string;
 }
