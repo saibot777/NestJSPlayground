98e9343882a210ce7c40c67cf0f46a4df939ec9a
saibot777 <stefantrajkovic777@gmail.com>
2019-09-18 15:23:01 +0200

    pipe status update

--- task-management/src/tasks/dto/pipes/task-status-validation.pipe.ts
+++ task-management/src/tasks/dto/pipes/task-status-validation.pipe.ts
@@ -1,11 +1,29 @@
-import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { TaskStatus } from '../../task.model';
 
 export class TaskStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    // tslint:disable-next-line: no-console
-    console.log(value, 'value');
-    // tslint:disable-next-line: no-console
-    console.log(metadata, 'metadata');
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
+    }
+
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
   }
 }
