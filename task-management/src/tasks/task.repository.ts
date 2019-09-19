8aa01e4d052ebe43e3865802623e01534807f578
saibot777 <stefantrajkovic777@gmail.com>
2019-09-19 10:32:18 +0200

    searh query impl

--- task-management/src/tasks/task.repository.ts
+++ task-management/src/tasks/task.repository.ts
@@ -10,6 +10,17 @@ export class TaskRepository extends Repository<Task> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
 
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'task.title LIKE :search OR task.description LIKE :search',
+        { search: `%${search}%` },
+      );
+    }
+
     const tasks = await query.getMany();
     return tasks;
   }
