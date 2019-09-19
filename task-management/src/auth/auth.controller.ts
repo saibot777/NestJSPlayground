0a37b726dccb5ab8a5ca249b5d73cdefcb506044
saibot777 <stefantrajkovic777@gmail.com>
2019-09-19 13:12:07 +0200

    response handling

--- task-management/src/auth/auth.controller.ts
+++ task-management/src/auth/auth.controller.ts
@@ -2,10 +2,13 @@ import {
   Body,
   ConflictException,
   Controller,
+  HttpStatus,
   InternalServerErrorException,
   Post,
+  Res,
   ValidationPipe,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -15,8 +18,18 @@ export class AuthController {
 
   @Post('/signup')
   public async signUp(
+    @Res() res: Response,
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ) {
-    return this.authService.signUp(authCredentialsDto);
+    try {
+      await this.authService.signUp(authCredentialsDto);
+      return res.status(HttpStatus.OK).json(['User created successfully']);
+    } catch (error) {
+      if (error.code === '23505' || 23505) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException(error);
+      }
+    }
   }
 }
--- task-management/src/auth/user.repository.ts
+++ task-management/src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -11,18 +12,12 @@ export class UserRepository extends Repository<User> {
   public async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    // const salt = bcrypt.genSalt();
+
     const user = new User();
     user.username = username;
     user.password = password;
 
-    try {
-      await user.save();
-    } catch (error) {
-      if (error.code === '23505') {
-        throw new ConflictException('Username already exists');
-      } else {
-        throw new InternalServerErrorException();
-      }
-    }
+    await user.save();
   }
 }
