664a911feb9847834f8a9b14e1010490115a2036
saibot777 <stefantrajkovic777@gmail.com>
2019-09-23 16:42:29 +0200

    configuration/conts initiated

---
+++ task-management/src/config/server-config.interface.ts
@@ -0,0 +1,5 @@
+export interface ServerConfig {
+  port: number;
+  type: string;
+  database: string;
+}
--- task-management/src/main.ts
+++ task-management/src/main.ts
@@ -1,11 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as config from 'config';
 import { AppModule } from './app.module';
+import { ServerConfig } from './config/server-config.interface';
 
 async function bootstrap() {
+  const serverConfig: ServerConfig = config.get('server');
   const logger = new Logger('bootstrap');
 
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   logger.log(`Application listening on port: ${port}`);
